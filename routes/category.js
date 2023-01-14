555ae609c4d66aaa11049962515db8269302d969
wjunxxx <wjun@88.com>
2023-01-15 02:30:40 +0800

    fix:解决nodejs中new Date()获取的时间比北京时间相差8小时的问题

--- routes/category.js
+++ routes/category.js
@@ -9,6 +9,7 @@ router.post('/addCategory', function (req, res, next) {
     let create_date = new Date().toLocaleString()
     console.log(create_date)
     connection.query('INSERT INTO pgblog.category(name,create_date) VALUE(?,?)',[req.body.name,create_date], function (err, rows, fields) {
+        console.log(err)
         if (err) {
             res.send({code:0,message:'操作失败'})
         } else {
@@ -42,7 +43,6 @@ router.post('/updateCategory', function (req, res, next) {
 // 查询分类列表
 router.get('/getCategoryList', function(req, res, next) {
     connection.query('SELECT * FROM pgblog.category', function (err, rows, fields) {
-        console.log(rows)
         res.send({code:1,data:rows,message:'请求成功'});
     })
 });
