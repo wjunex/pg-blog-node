2ebd03d44dbd986391f7d89032695abf745ce38e
wjunxxx <wjun@88.com>
2023-01-15 01:51:39 +0800

    数据库时区配置

--- sql/index.js
+++ sql/index.js
@@ -3,7 +3,8 @@ var connection = mysql.createConnection({
     host: '139.9.146.62',
     user: 'root',
     password: '123456',
-    database: 'pgblog'
+    database: 'pgblog',
+    timezone: '08:00'
 })
 
 connection.connect()
